5456305b8817493b53d1ab33465b9b20678cad0b
Omar Daoud <omardaoud.dev@gmail.com>
2023-10-12 15:41:30 -0400

    "Refactored code to capitalize first letter in each name"

--- FullNameGenerator/src/main/java/org/example/Main.java
+++ FullNameGenerator/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String message= "Yikes";
+       /* String message= "Yikes";
         System.out.println(message);
 
         for (int i =0; i<message.length(); i++){
@@ -13,25 +13,31 @@ public class Main {
         }
         for (int i= message.length()-1; i>= 0; i--){
             System.out.println(message.charAt(i));
+        }*/
+
+        String first = getStringInput(scanner,"Enter your first name: ").trim();
+        String last = getStringInput(scanner, "Enter your last name: ").trim();
+        String middle = getStringInput(scanner, "Enter your middle name: ").trim();
+        String title= getStringInput(scanner, "Enter your title: ").trim();
+
+
+
+       middle =(middle.length()==1) ? middle + '.': middle;
+
+        if (! title.isBlank()) {
+           System.out.println(capitalizeLetter(first) + " " + capitalizeLetter(middle) + " " + capitalizeLetter(last) + ", " + capitalizeLetter(title));
+        }else {
+            System.out.println(capitalizeLetter(first) + " " + capitalizeLetter(middle) + " " + capitalizeLetter(last) );
         }
-
-        String first = getStringInput(scanner,"Enter your first name: ");
-        String last = getStringInput(scanner, "Enter youjr first name");
-        String middle = getStringInput(scanner, "Enter your middle name");
-        String title= getStringInput(scanner, "Enter your title");
-
-        System.out.println(first + middle + last + title);
-
-        if (middle.length()==1 ) middle= middle + ".";
-        if (! title.isEmpty() ) title = ", " + title;
-
-        System.out.println(first + " " + middle + " "+ last + title);
     }
 
     public static String getStringInput (Scanner scanner, String prompt){
         System.out.print(prompt);
-        String input = scanner.nextLine();
-        return input;
+        return scanner.nextLine();
 
+
+    }
+    public static String capitalizeLetter(String name){
+        return Character.toUpperCase(name.charAt(0)) + name.substring(1);
     }
 }
